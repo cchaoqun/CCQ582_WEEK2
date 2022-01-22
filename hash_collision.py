e447f14580b138b2409a938d08f067473b180b7f
ChaoqunCheng <chengchaoqun@hotmail.com>
2022-01-22 13:03:47 +0800

    finish

---
+++ hash_collision.py
@@ -0,0 +1,33 @@
+import hashlib
+import os
+
+
+def hash_collision(k):
+    if not isinstance(k, int):
+        print("hash_collision expects an integer")
+        return (b'\x00', b'\x00')
+    if k < 0:
+        print("Specify a positive number of bits")
+        return (b'\x00', b'\x00')
+
+    # Collision finding code goes here
+    # Generate 2 random bytes of 32 bits
+    INT_BYTE_LEN = 4
+    x = get_random_bytes(INT_BYTE_LEN)
+    y = get_random_bytes(INT_BYTE_LEN)
+
+    while(True):
+        if get_last_k_bits(INT_BYTE_LEN, x) == get_last_k_bits(INT_BYTE_LEN, y):
+            return (x, y)
+    return (b'\x00', b'\x00')
+
+
+def get_random_bytes(byte):
+    return os.urandom(byte)
+
+def get_last_k_bits(k, bytes):
+    kMask = pow(2, k) - 1
+    bytes = hashlib.sha256(bytes)
+    bits = int(bytes.hexdigest(), 16)
+    return (bits & kMask)
+
