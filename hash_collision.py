9f6c077b71a2063e8fa34ad2ca71ac4c48d50bef
ChaoqunCheng <chengchaoqun@hotmail.com>
2022-01-22 13:18:36 +0800

    finsih

--- hash_collision.py
+++ hash_collision.py
@@ -13,10 +13,10 @@ def hash_collision(k):
     # Collision finding code goes here
     # Generate 2 random bytes of 32 bits
     INT_BYTE_LEN = 4
-    x = get_random_bytes(INT_BYTE_LEN)
-    y = get_random_bytes(INT_BYTE_LEN)
 
     while(True):
+        x = get_random_bytes(INT_BYTE_LEN)
+        y = get_random_bytes(INT_BYTE_LEN)
         if get_last_k_bits(INT_BYTE_LEN, x) == get_last_k_bits(INT_BYTE_LEN, y):
             return (x, y)
     return (b'\x00', b'\x00')
@@ -26,7 +26,7 @@ def get_random_bytes(byte):
     return os.urandom(byte)
 
 def get_last_k_bits(k, bytes):
-    kMask = pow(2, k) - 1
+    kMask = 2 ** k - 1
     bytes = hashlib.sha256(bytes)
     bits = int(bytes.hexdigest(), 16)
     return (bits & kMask)
